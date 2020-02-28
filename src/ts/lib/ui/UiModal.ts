ef72bdf627243177948c403524b679ebcc033446
Dan <k.daniel.adams@gmail.com>
2020-02-27 20:11:10 -0500

    add modal library and test button

--- src/ts/lib/ui/UiModal.ts
+++ src/ts/lib/ui/UiModal.ts
@@ -1,18 +1,34 @@
+import * as _swal from 'sweetalert';
+import { SweetAlert } from 'sweetalert/typings/core';
+
+const swal: SweetAlert = _swal as any;
+
 export class UiModal {
-    width: number;
-    height: number;
-    x: number;
-    y: number;
     title: string;
     message: string;
+    message_element: HTMLElement;
 
-    constructor(title: string, msg: string, width: number, height: number, x: number, y: number) 
-    {
-        this.title = title;
-        this.message = msg;
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
+    constructor();
+    constructor(title: string, msg: HTMLElement);
+    constructor(title: string, msg: string);
+    constructor(title?: string, msg?: any) {
+        if(title) 
+            this.title = title;
+        
+        if(typeof(msg) == 'string') 
+            this.message = msg;
+        else if(msg instanceof HTMLElement) 
+            this.message_element = msg;
+    }
+
+    click(event: MouseEvent) {
+        if(!this.message_element)
+            swal(this.title, this.message);
+        else {
+            swal({
+                title: this.title,
+                content: { element: this.message_element }
+            });
+        }
     }
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,6 +2,7 @@ import '../css/styles.css';
 import '../css/typechess.css';
 import * as config from './config.json';
 import { Typechess } from './lib/Typechess';
+import { UiModal } from './lib/ui/UiModal';
 
 /**
  * Start
@@ -17,5 +18,26 @@ window.addEventListener('load', (load_event) => {
 
     canvas.addEventListener('click', (event) => {
         match.click(event);
+    });
+
+    document.getElementById("test_swal").addEventListener('click', (e) => {
+        let title: string = "Testing...",
+            msg: HTMLDivElement = document.createElement("div"),
+            modal: UiModal; 
+        
+        msg.innerHTML = `
+            Choose one: <br>
+            <input type="radio" name="savegame" value="testing1" id="testing1">
+            <label for="testing1">Testing 1</label>
+            <br>
+            <input type="radio" name="savegame" value="testing2" id="testing2">
+            <label for="testing2">Testing 2</label>
+            <br>
+            <input type="radio" name="savegame" value="testing3" id="testing3">
+            <label for="testing3">Testing 3</label>
+        `;
+
+        modal = new UiModal(title, msg);
+        modal.click(e);
     });
 });
